16557465fd20a4b70acd270c79e2077e6087e189
Васин Кирилл Романович <Vasin.K.R@sberbank.ru>
2019-02-07 16:18:43 +0300

    Person dtos modified
    - added toString
    - removed setters

--- person-info-service-api/src/main/java/ru/gridr/springcloudexample/person/PersonDto.java
+++ person-info-service-api/src/main/java/ru/gridr/springcloudexample/person/PersonDto.java
@@ -35,15 +35,15 @@ public class PersonDto {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public String toString() {
+        return "PersonDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- person-info-service/src/main/java/ru/gridr/springcloudexample/personinfo/service/Person.java
+++ person-info-service/src/main/java/ru/gridr/springcloudexample/personinfo/service/Person.java
@@ -50,15 +50,16 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
